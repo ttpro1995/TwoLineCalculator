527059c43ac3023d8138d79f0a2d5eafa7ec3a43
thien.thai <tthien@apcs.vn>
2017-04-04 13:43:06 +0700

    fix calculation

--- src/Calculator/Calculation.java
+++ src/Calculator/Calculation.java
@@ -21,34 +21,29 @@ public class Calculation {
     }
     
     public static double plus(String a, String b){
-        String[] part_a = a.split("/");
-        String[] part_b = b.split("/");
-        Fraction fraction_a = Fraction.getReducedFraction(Integer.parseInt(part_a[0]), Integer.parseInt(part_a[1]));
-        Fraction fraction_b = Fraction.getReducedFraction(Integer.parseInt(part_b[0]), Integer.parseInt(part_b[1]));
+        FractionFormat ff = new FractionFormat();
+        Fraction fraction_a = ff.parse(a);
+        Fraction fraction_b = ff.parse(b);
         double d = fraction_a.doubleValue() + fraction_b.doubleValue();
         return d;
     }
     public static double minus(String a, String b){
-        String[] part_a = a.split("/");
-        String[] part_b = b.split("/");
-        Fraction fraction_a = Fraction.getReducedFraction(Integer.parseInt(part_a[0]), Integer.parseInt(part_a[1]));
-        Fraction fraction_b = Fraction.getReducedFraction(Integer.parseInt(part_b[0]), Integer.parseInt(part_b[1]));
-        double d = fraction_a.doubleValue() - fraction_b.doubleValue();
+        FractionFormat ff = new FractionFormat();
+        Fraction fraction_a = ff.parse(a);
+        Fraction fraction_b = ff.parse(b);        double d = fraction_a.doubleValue() - fraction_b.doubleValue();
         return d;
     }
     public static double mul(String a, String b){
-        String[] part_a = a.split("/");
-        String[] part_b = b.split("/");
-        Fraction fraction_a = Fraction.getReducedFraction(Integer.parseInt(part_a[0]), Integer.parseInt(part_a[1]));
-        Fraction fraction_b = Fraction.getReducedFraction(Integer.parseInt(part_b[0]), Integer.parseInt(part_b[1]));
+        FractionFormat ff = new FractionFormat();
+        Fraction fraction_a = ff.parse(a);
+        Fraction fraction_b = ff.parse(b);
         double d = fraction_a.doubleValue() * fraction_b.doubleValue();
         return d;
     }
     public static double div(String a, String b){
-        String[] part_a = a.split("/");
-        String[] part_b = b.split("/");
-        Fraction fraction_a = Fraction.getReducedFraction(Integer.parseInt(part_a[0]), Integer.parseInt(part_a[1]));
-        Fraction fraction_b = Fraction.getReducedFraction(Integer.parseInt(part_b[0]), Integer.parseInt(part_b[1]));
+        FractionFormat ff = new FractionFormat();
+        Fraction fraction_a = ff.parse(a);
+        Fraction fraction_b = ff.parse(b);
         double d = fraction_a.doubleValue() / fraction_b.doubleValue();
         return d;
     }
